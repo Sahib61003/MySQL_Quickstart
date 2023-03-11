30a599780b0d979c36255c050c4d009790f93d29
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-03-11 18:41:30 +0530

    Updated

--- Leetcode_Study_Plan/String Processing Functions (Day3)/fix_name_in_a_table.sql
+++ Leetcode_Study_Plan/String Processing Functions (Day3)/fix_name_in_a_table.sql
@@ -1,1 +1,21 @@
+/*
+Table: Users
 
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+*/
+
+SELECT user_id, 
+concat(upper(substr(name, 1, 1)), lower(substr(name, 2, length(name)-1))) as name 
+from Users order by user_id;
