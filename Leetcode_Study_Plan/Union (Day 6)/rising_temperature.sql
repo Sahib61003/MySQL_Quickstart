033662276f06a79046da330dc7738f6659acbeb1
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-04 23:00:12 +0530

    Added

--- Leetcode_Study_Plan/Union (Day 6)/rising_temperature.sql
+++ Leetcode_Study_Plan/Union (Day 6)/rising_temperature.sql
@@ -1,1 +1,22 @@
+/*
+Table: Weather
 
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+*/
+
+SELECT A.id FROM weather A, weather B
+WHERE datediff(A.recordDate, B.recordDate)= 1
+AND A.temperature > B.temperature;
