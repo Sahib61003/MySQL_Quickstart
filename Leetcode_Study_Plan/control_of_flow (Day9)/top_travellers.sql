96cecd83351cdd8cae77a1347b375c18078ae337
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-13 00:14:56 +0530

    Created

--- Leetcode_Study_Plan/control_of_flow (Day9)/top_travellers.sql
+++ Leetcode_Study_Plan/control_of_flow (Day9)/top_travellers.sql
@@ -1,1 +1,36 @@
+/*
+Table: Users
 
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key for this table.
+name is the name of the user.
+ 
+
+Table: Rides
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| user_id       | int     |
+| distance      | int     |
++---------------+---------+
+id is the primary key for this table.
+user_id is the id of the user who traveled the distance "distance".
+ 
+
+Write an SQL query to report the distance traveled by each user.
+
+Return the result table ordered by travelled_distance in descending order, if two or more users traveled the same distance, order them by their name in ascending order.
+
+*/
+select distinct u.name, case when r.distance is not null then sum(r.distance) 
+over(partition by r.user_id order by r.user_id) 
+else 0 end as travelled_distance 
+from Users u left join Rides r on u.id=r.user_id
+ order by travelled_distance desc, u.name;
