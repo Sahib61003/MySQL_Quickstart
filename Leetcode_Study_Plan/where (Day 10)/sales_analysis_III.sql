4ca7720fd6f565eec30357a998b8ce14ce5780ac
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-14 00:22:14 +0530

    Added

--- Leetcode_Study_Plan/where (Day 10)/sales_analysis_III.sql
+++ Leetcode_Study_Plan/where (Day 10)/sales_analysis_III.sql
@@ -31,4 +31,8 @@ Write an SQL query that reports the products that were only sold in the first qu
 
 Return the result table in any order.
 */
-
+elect s.product_id, p.product_name
+from sales s, product p
+where s.product_id = p.product_id
+group by s.product_id, p.product_name
+having min(s.sale_date) >= '2019-01-01' AND max(s.sale_date) <= '2019-03-31';
